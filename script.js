ddef534151dda5a66f0f28d722fd4b471772a54f
Shickato <jortresroeltorp@gmail.com>
2026-04-15 13:14:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const urls = [
     'https://shickat.me',
     'https://wikishickat.pages.dev/',
     'https://36f0bea8.wikishickat.pages.dev/',
-    'https://ipfs.io/ipns/k51qzi5uqu5dgkcvogpofvprp6i8i2rtr6ej0li3ueqimgxw3803gjoqhar1uw/',
-    'https://k51qzi5uqu5dgkcvogpofvprp6i8i2rtr6ej0li3ueqimgxw3803gjoqhar1uw.ipns.dweb.link/',
-    'https://ipfs.io/ipfs/QmWAjHLzCTv12WkH9ntuUsUii2Cw1an4sAwHuV4EAgnqzw/',
+    'https://ipfs.io/ipns/k51qzi5uqu5djuh4f1w0oladi6rxgf2lkbocof3c1u5mz5k0m3or0lqknppcwa',
+    'https://k51qzi5uqu5djuh4f1w0oladi6rxgf2lkbocof3c1u5mz5k0m3or0lqknppcwa.ipns.dweb.link',
+    'https://ipfs.io/ipfs/QmWAjHLzCTv12WkH9ntuUsUii2Cw1an4sAwHuV4EAgnqzw',
     'https://shickat.blog',
     'https://shickatblog.pages.dev/',
     'https://3d159209.shickatblog.pages.dev/'
